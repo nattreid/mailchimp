8140380d1aa8568a5c910e7d5e7a986566c4499d
attreid <attreid@gmail.com>
2018-02-05 12:59:56 +0100

    Fixed #1

---
+++ src/DI/AbstractMailChimpExtension.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NAttreid\MailChimp\DI;
+
+use NAttreid\Cms\Configurator\Configurator;
+use NAttreid\Cms\DI\ExtensionTranslatorTrait;
+use NAttreid\MailChimp\Hooks\MailChimpHook;
+use NAttreid\MailChimp\MailChimpClient;
+use NAttreid\WebManager\Services\Hooks\HookService;
+use Nette\DI\CompilerExtension;
+use Nette\DI\Helpers;
+use Nette\DI\ServiceDefinition;
+use Nette\DI\Statement;
+
+/**
+ * Class AbstractMailChimpExtension
+ *
+ * @author Attreid <attreid@gmail.com>
+ */
+abstract class AbstractMailChimpExtension extends CompilerExtension
+{
+	private $defaults = [
+		'apiKey' => null,
+		'listId' => null,
+		'debug' => false,
+		'store' => null,
+		'tempDir' => '%tempDir%'
+	];
+
+	public function loadConfiguration(): void
+	{
+		$builder = $this->getContainerBuilder();
+		$config = $this->validateConfig($this->defaults, $this->getConfig());
+
+		$mailChimpConfig = $builder->addDefinition($this->prefix('config'))
+			->setType(MailChimpConfig::class)
+			->addSetup(new Statement('$service->apiKey = ?', [$config['apiKey']]))
+			->addSetup(new Statement('$service->listId = ?', [$config['listId']]));
+
+		$store = $config['store'];
+		if ($store !== null) {
+			$mailChimpStore = $builder->addDefinition($this->prefix('store'))
+				->setType(MailChimpStore::class)
+				->addSetup(new Statement('$service->id = ?', [$store['id'] ?? null]))
+				->addSetup(new Statement('$service->name = ?', [$store['name']]))
+				->addSetup(new Statement('$service->domain = ?', [$store['domain']]))
+				->addSetup(new Statement('$service->email = ?', [$store['email']]))
+				->addSetup(new Statement('$service->currency = ?', [$store['currency']]));
+
+			if (isset($store['id'])) {
+				$mailChimpConfig->addSetup(new Statement('$service->store = ?', [$mailChimpStore]));
+			}
+		}
+
+		$mailChimpConfig = $this->prepareHook($mailChimpConfig);
+
+		$tempDir = Helpers::expand($config['tempDir'], $builder->parameters);
+
+		$builder->addDefinition($this->prefix('client'))
+			->setType(MailChimpClient::class)
+			->setArguments([$config['debug'], $mailChimpConfig, $tempDir]);
+	}
+
+	protected function prepareHook(ServiceDefinition $mailChimpConfig)
+	{
+		return $mailChimpConfig;
+	}
+}
--- src/DI/MailChimpExtension.php
+++ src/DI/MailChimpExtension.php
@@ -7,71 +7,35 @@ namespace NAttreid\MailChimp\DI;
 use NAttreid\Cms\Configurator\Configurator;
 use NAttreid\Cms\DI\ExtensionTranslatorTrait;
 use NAttreid\MailChimp\Hooks\MailChimpHook;
-use NAttreid\MailChimp\MailChimpClient;
 use NAttreid\WebManager\Services\Hooks\HookService;
-use Nette\DI\CompilerExtension;
-use Nette\DI\Helpers;
+use Nette\DI\ServiceDefinition;
 use Nette\DI\Statement;
 
-/**
- * Class MailChimpExtension
- *
- * @author Attreid <attreid@gmail.com>
- */
-class MailChimpExtension extends CompilerExtension
-{
-	use ExtensionTranslatorTrait;
-
-	private $defaults = [
-		'apiKey' => null,
-		'listId' => null,
-		'debug' => false,
-		'store' => null,
-		'tempDir' => '%tempDir%'
-	];
-
-	public function loadConfiguration(): void
+if (trait_exists('NAttreid\Cms\DI\ExtensionTranslatorTrait')) {
+	class MailChimpExtension extends AbstractMailChimpExtension
 	{
-		$builder = $this->getContainerBuilder();
-		$config = $this->validateConfig($this->defaults, $this->getConfig());
+		use ExtensionTranslatorTrait;
 
-		$mailChimpConfig = $builder->addDefinition($this->prefix('config'))
-			->setType(MailChimpConfig::class)
-			->addSetup(new Statement('$service->apiKey = ?', [$config['apiKey']]))
-			->addSetup(new Statement('$service->listId = ?', [$config['listId']]));
+		protected function prepareHook(ServiceDefinition $mailChimpConfig)
+		{
+			$builder = $this->getContainerBuilder();
+			$hook = $builder->getByType(HookService::class);
+			if ($hook) {
+				$builder->addDefinition($this->prefix('mailChimpHook'))
+					->setType(MailChimpHook::class)
+					->addSetup('setConfig', [$mailChimpConfig, '@' . MailChimpStore::class]);
 
-		$store = $config['store'];
-		if ($store !== null) {
-			$mailChimpStore = $builder->addDefinition($this->prefix('store'))
-				->setType(MailChimpStore::class)
-				->addSetup(new Statement('$service->id = ?', [$store['id'] ?? null]))
-				->addSetup(new Statement('$service->name = ?', [$store['name']]))
-				->addSetup(new Statement('$service->domain = ?', [$store['domain']]))
-				->addSetup(new Statement('$service->email = ?', [$store['email']]))
-				->addSetup(new Statement('$service->currency = ?', [$store['currency']]));
+				$this->setTranslation(__DIR__ . '/../lang/', [
+					'webManager'
+				]);
 
-			if (isset($store['id'])) {
-				$mailChimpConfig->addSetup(new Statement('$service->store = ?', [$mailChimpStore]));
+				$mailChimpConfig = new Statement('?->mailChimp \?: ?', ['@' . Configurator::class, '@' . MailChimpConfig::class]);
 			}
+			return $mailChimpConfig;
 		}
-
-		$hook = $builder->getByType(HookService::class);
-		if ($hook) {
-			$builder->addDefinition($this->prefix('mailChimpHook'))
-				->setType(MailChimpHook::class)
-				->addSetup('setConfig', [$mailChimpConfig, '@' . MailChimpStore::class]);
-
-			$this->setTranslation(__DIR__ . '/../lang/', [
-				'webManager'
-			]);
-
-			$mailChimpConfig = new Statement('?->mailChimp \?: ?', ['@' . Configurator::class, '@' . MailChimpConfig::class]);
-		}
-
-		$tempDir = Helpers::expand($config['tempDir'], $builder->parameters);
-
-		$builder->addDefinition($this->prefix('client'))
-			->setType(MailChimpClient::class)
-			->setArguments([$config['debug'], $mailChimpConfig, $tempDir]);
+	}
+} else {
+	class MailChimpExtension extends AbstractMailChimpExtension
+	{
 	}
 }
